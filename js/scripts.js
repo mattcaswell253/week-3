4671f7831724e57ad76219171d65b7804eefe1d8
mattcaswell <matthewcaswell253@Gmail.com>
2017-01-20 11:50:06 -0800

    complete second spec of replacing multiples of 3 with ping

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 //user logic
 $(document).ready(function() {
 	$("form#pingpong").submit(function(event) {
+
+    //business logic
 		var number = parseInt($('input#firstNum').val());
 		for (var index = 1; index <= number; index += 1) {
-   {
-				$('ul#list').append("<li>" + index + "</li>");
+      if (index % 3 === 0) {
+        $('ul#list').append("<li>ping</li>");
+      }
+   {	$('ul#list').append("<li>" + index + "</li>");
 			}
 		};
     event.preventDefault();
