168d691fd14c9dc5be72a8461a0eea9f3e824c4c
mattcaswell <matthewcaswell253@Gmail.com>
2017-01-20 11:41:49 -0800

    complete fix of first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,13 @@
-var result = function(number) {
-var count;
-for(count = 0; count <= number; count++){
-  document.write(count + ",");
-  document.write("<br/>")
-  }
-}
-
-
-
 //user logic
 $(document).ready(function() {
-  $("form#pingpong").submit(function(event) {
-  event.preventDefault();
-  var number = parseInt($("input#firstNum").val());
-
-  var answer = result(number);
-  $(".result").text(answer);
-
-  })
-})
+	$("form#pingpong").submit(function(event) {
+		var number = parseInt($('input#firstNum').val());
+		for (var index = 1; index <= number; index += 1) {
+   {
+				$('ul#list').append("<li>" + index + "</li>");
+			}
+		};
+    event.preventDefault();
+		$('#results').show()
+	});
+});
