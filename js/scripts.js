330056a74413af414d7f02b164f034cfcf71a75c
mattcaswell <matthewcaswell253@Gmail.com>
2017-01-20 12:45:57 -0800

    complete fourth spec of replacing multiples of 15 with ping-ponng

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
 
     //business logic
     var number = parseInt($('input#firstNum').val());
-    for (var index = 1; index <= number; index += 1) 
+    for (var index = 1; index <= number; index += 1) {
+      if ((index % 3 === 0) && (index % 5 ===0)) {
+        $('ul#list').append("<li>ping-pong</li>");
       } else if (index % 3 === 0) {
         $('ul#list').append("<li>ping</li>");
       } else if (index % 5 === 0) {
